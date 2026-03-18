fa4575543e99ad82ce152b8724c26fa964ae0550
juanMasis777 <juanmasisg@gmail.com>
2026-03-18 05:27:28 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let btn = document.getElementById("btn-generate");
+  let input = document.getElementById("input-base");
+  let output = document.getElementById("output");
+
+  btn.onclick = function () {
+    let name = (input.value || "hola").trim().toLowerCase().split(".")[0];
+    let tlds = [".com", ".net", ".org", ".es", ".us", ".shop", ".ca", ".io"];
+    let result = "";
+
+    for (let t of tlds) {
+      let domain = name + t;
+      console.log(domain);
+      result += domain + "\n";
+    }
+
+    output.textContent = result;
+  };
 };
